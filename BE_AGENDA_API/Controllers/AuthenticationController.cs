6ae5db53dde2d8dcf747e8a23e1d154bd8f5450e
brunoDiter <bruno.diterlizzi@gmail.com>
2023-02-28 21:08:40 -0300

    Creacion de controlador Authentication

---
+++ BE_AGENDA_API/Controllers/AuthenticationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE_AGENDA_API.Controllers
+{
+    public class AuthenticationController : ControllerBase
+    {
+
+    }
+}
